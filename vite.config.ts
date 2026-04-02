28cf64386e4574e1b80edd85a944d1e6d89d57d7
maximov-dev <iamyaroslavmaximov@gmail.com>
2026-04-03 00:23:57 +0300

    feat(feature1): deployment to gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from "path"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-    resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+   plugins: [react(), tailwindcss()],
+   base: '/personal-website',
+   resolve: {
+      alias: {
+         '@': path.resolve(__dirname, './src'),
+      },
+   },
+});
